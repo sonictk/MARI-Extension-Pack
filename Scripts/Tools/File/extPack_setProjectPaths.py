353943c8305e4c1de62f3bf59efc56d79f8f7d19
campi3d <MariIdeas@campi3d.com>
2015-08-15 12:45:17 +1200

    projectSetter v0.7: path variable setting works (again)
    - All  functionality works now
    - still need to get all paths saved between sessions

--- Scripts/Tools/File/extPack_setProjectPaths.py
+++ Scripts/Tools/File/extPack_setProjectPaths.py
@@ -765,92 +765,35 @@ class setProjectPathUI(QtGui.QDialog):
             else:
                 return
 
+        self._setPath(base_var_final_dict)
 
 
 
 
-
-
-
-
-
-
-
-        # self.accept()
-
-        else:
-            print 'all good'
-            return
-
-#
-        # variable_path = dict_val_01.text()
-#
-        # resolved_val = variable_path.replace('$BASE', base_val)
-        # return resolved_val
-
-
-    def _setPath(self):
+    def _setPath(self,var_dict):
         """ Sets the new value for activated variables"""
 
+        print 'SET PROJECT performed the following actions:\n'
 
-        # Set Texture Import Path
-        if self.Active_VarA.isChecked:
-            pathA = self._checkInput(self.Path_VarA)
-            mari.resources.setPath(mari.resources.DEFAULT_IMPORT,pathA)
-        # Set Texture Export Path
-        if self.Active_VarC.isChecked:
-            pathC = self._checkInput(self.Path_VarC)
-            mari.resources.setPath(mari.resources.DEFAULT_EXPORT,pathC)
-        # Set Geo Path
-        if self.Active_VarD.isChecked:
-            pathD = self._checkInput(self.Path_VarD)
-            mari.resources.setPath(mari.resources.DEFAULT_GEO,pathD)
-        # Set Tmage Manager Path
-        if self.Active_VarE.isChecked:
-            pathE = self._checkInput(self.Path_VarE)
-            mari.resources.setPath(mari.resources.DEFAULT_IMAGE,pathE)
-        # Set Render Path
-        if self.Active_VarF.isChecked:
-            pathF = self._checkInput(self.Path_VarF)
-            mari.resources.setPath(mari.resources.DEFAULT_RENDER,pathF)
-        # Set Archive Path
-        if self.Active_VarG.isChecked:
-            pathG = self._checkInput(self.Path_VarG)
-            mari.resources.setPath(mari.resources.DEFAULT_ARCHIVE,pathG)
-        # Set Shelf Path
-        if self.Active_VarH.isChecked:
-            pathH = self._checkInput(self.Path_VarH)
-            mari.resources.setPath(mari.resources.DEFAULT_SHELF,pathH)
-        # Set Camera Path
-        if self.Active_VarI.isChecked:
-            pathI = self._checkInput(self.Path_VarI)
-            mari.resources.setPath(mari.resources.DEFAULT_CAMERA,pathI)
-        # Set Texture Flattened Temaplte
-        if self.Active_VarJ.isChecked:
-            mari.resources.setFlattenedSequenceTemplate(self.Path_VarJ.text())
-        # Set Texture Temaplte
-        if self.Active_VarK.isChecked:
-            mari.resources.setSequenceTemplate(self.Path_VarK.text())
-        # Set PTEX Flattened Temaplte
-        if self.Active_VarL.isChecked:
-            mari.resources.setPtexFlattenedSequenceTemplate(self.Path_VarL.text())
-        # Set PTEX Temaplte
-        if self.Active_VarM.isChecked:
-            mari.resources.setPtexSequenceTemplate(self.Path_VarM.text())
+        for key in var_dict:
+            dict_ACTIVE = var_dict[key][0] # Is Field Active ?
+            dict_PATHTEXT = var_dict[key][1] # What is the PathText of the Active Field
+            dict_VARIABLE = var_dict[key][2] # The Variable associated to the Field
+            dict_BASEVAR = var_dict[key][3] # Is there a $BASE Variable in the Path ?
+            dict_DIREXIST = var_dict[key][4] # Does the resolved Path exist ?
+            dict_RESOLVEDPATH = var_dict[key][5] # Final resolved Path (converted $BASE Variable)
 
 
-        # print mari.resources.path(mari.resources.DEFAULT_EXPORT)
-        # print mari.resources.path(mari.resources.DEFAULT_IMPORT)
-        # print mari.resources.path(mari.resources.DEFAULT_ARCHIVE)
-        # print mari.resources.path(mari.resources.DEFAULT_CAMERA)
-        # print mari.resources.path(mari.resources.DEFAULT_GEO)
-        # print mari.resources.path(mari.resources.DEFAULT_IMAGE)
-        # print mari.resources.path(mari.resources.DEFAULT_SHELF)
-        # print mari.resources.path(mari.resources.DEFAULT_RENDER)
-        # print mari.resources.sequenceTemplate()
-        # print mari.resources.flattenedSequenceTemplate()
-        # print mari.resources.ptexSequenceTemplate()
-        # print mari.resources.ptexFlattenedSequenceTemplate()
+
+
+            if dict_ACTIVE:
+                try:
+                    mari.resources.setPath(dict_VARIABLE,dict_RESOLVEDPATH)
+                    print dict_VARIABLE + ' set to: ' + dict_RESOLVEDPATH
+                except Exception:
+                    mari.utils.message('Unable to set path for Variable: \n' + key)
+                    pass
+
 
         self.accept()
 
